37459ed06fbe676667edd3d3b05d7a41d4fdca19
Michael Deng <michaeldeng55@gmail.com>
2023-09-15 18:49:05 -0400

    geolocation api added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  
+  <h1>Accuenergy Canada</h1>
+  <LocationButton/>
 </template>
 
 <script>
+import LocationButton from "./components/LocationButton.vue"
 export default {
-
+components: {LocationButton}
 }
 </script>
 
 <style>
-
+  #app {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
---
+++ src/components/LocationButton.vue
@@ -0,0 +1,45 @@
+<template>
+<div class = 'location-container'>
+  <button @click = "findLocation">Find Location</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+
+    let localLat = ref(null)
+    let localLong = ref(null)
+
+    const findLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          localLat.value = position.coords.latitude;
+          localLong.value = position.coords.longitude;
+      })
+      }
+      else {
+        console.log('error');
+      }
+    }
+
+    return {
+      findLocation,
+      localLat,
+      localLong
+    }
+
+
+  }
+
+}
+</script>
+
+<style scoped>
+.location-container {
+  width: 15rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
