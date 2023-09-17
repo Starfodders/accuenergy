5b74bf74f3536f03cc4f5d55db5a6924ad1e51dd
Michael Deng <michaeldeng55@gmail.com>
2023-09-16 20:18:59 -0400

    success in removing table items

--- src/components/LocationList.vue
+++ src/components/LocationList.vue
@@ -1,23 +1,45 @@
 <script>
 import { watch, ref } from "vue";
-import LocationListItem from "./LocationListItem.vue"
+import LocationListItem from "./LocationListItem.vue";
 
 export default {
-  props: ['updateList'],
-  components: {LocationListItem},
+  props: ["updateList"],
+  components: { LocationListItem },
   setup(props) {
-    
     //when there is a new location, push into locations array which then updates the LocationListItem
-    const locationsArray = ref([])
+    const locationsArray = ref([]);
+    const deleteArray = ref([]);
 
+    watch(
+      () => props.updateList,
+      () => {
+        locationsArray.value.push(props.updateList);
+      }
+    );
 
-    watch(() => props.updateList, () => {
-       locationsArray.value.push(props.updateList)
+    const handleAdd = (id) => {
+      deleteArray.value.push(id);
+    };
+    const handleRemove = (id) => {
+      const index = deleteArray.value.indexOf(id);
+      if (index !== -1) {
+        deleteArray.value.splice(index, 1);
+      }
+    };
 
-    })
+    const deleteSelected = () => {
+      locationsArray.value = locationsArray.value.filter(
+        (location) => !deleteArray.value.includes(location.id)
+      );
+      deleteArray.value = [];
+    };
 
     return {
-        locationsArray
+      locationsArray,
+      deleteArray,
+      handleAdd,
+      handleRemove,
+      deleteSelected,
     };
   },
 };
@@ -26,10 +48,17 @@ export default {
 <template>
   <div class="list-container">
     <div class="list-container-top">
-        <button>Remove Selected</button>
+      <button @click="deleteSelected">Remove Selected</button>
     </div>
     <div class="list-container-main">
-        <LocationListItem v-for = "location in locationsArray" :key = "location.id" :details = "location"/>
+      <LocationListItem
+        v-for="location in locationsArray"
+        :key="location.id"
+        :details="location"
+        :deleteArray="deleteArray"
+        @addToArray="handleAdd"
+        @removeFromArray="handleRemove"
+      />
     </div>
   </div>
 </template>
@@ -40,13 +69,15 @@ export default {
   max-width: 25em;
   display: flex;
   flex-direction: column;
+  padding: 0 1em;
 }
 .list-container-top {
-    width: 100%;
-    background-color: red;
+  width: 100%;
+  background-color: red;
 }
 .list-container-main {
-    width: 100%;
-    display: flex;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/components/LocationListItem.vue
+++ src/components/LocationListItem.vue
@@ -2,29 +2,65 @@
 import { ref, onMounted } from "vue";
 
 export default {
-  props: ["details"],
-  setup(props) {
+  props: ["details", "deleteArray"],
+  setup(props, {emit}) {
+    const id = ref(null);
     const address = ref(null);
+    const isChecked = ref(false);
+
     onMounted(() => {
-      console.log("Props passed to child:", props.details);
       address.value = props.details.name;
+      id.value = props.details.id;
     });
+
+    const toggleDelete = () => {
+      if (!isChecked.value) {
+        if (!props.deleteArray.includes(props.details.id)) {
+          emit("addToArray", props.details.id);
+        }
+      } else {
+        const index = props.deleteArray.indexOf(props.details.id);
+        if (index !== -1) {
+          emit("removeFromArray", props.details.id);
+
+        }
+      }
+    };
 
     return {
       address,
+      id,
+      toggleDelete,
+      isChecked,
     };
   },
 };
 </script>
 
 <template>
-  <div class="item-checkbox">
-    <input type="checkbox" />
-  </div>
-  <div class="item-address">
-    <span>{{ address }}</span>
-  </div>
+  <section class="item" ref="id">
+    <div class="item-checkbox">
+      <input type="checkbox" @click="toggleDelete" v-model="isChecked" />
+    </div>
+    <div class="item-address">
+      <span>{{ address }}</span>
+    </div>
+  </section>
 </template>
 
 <style>
+.item {
+  display: flex;
+  width: 100%;
+  background-color: rgb(219, 219, 219);
+}
+.item-checkbox {
+  padding: 1em 1em 1em 0.5em;
+  border-right: 1px solid black;
+}
+.item-address {
+  padding: 0 1em;
+  display: flex;
+  align-items: center;
+}
 </style>
